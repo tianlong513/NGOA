55f33309506983f3fcd828cf59b78e112d11bb01
tianlong513 <44767147+tianlong513@users.noreply.github.com>
2019-01-12 16:21:28 +0800

    提交WebController，一些认证模块

---
+++ src/main/java/org/oa/controller/WebController.java
@@ -0,0 +1,84 @@
+package org.oa.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authz.annotation.Logical;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.apache.shiro.authz.annotation.RequiresRoles;
+import org.apache.shiro.subject.Subject;
+import org.oa.bean.ResponseBean;
+import org.oa.database.UserBean;
+import org.oa.database.UserService;
+import org.oa.exception.UnauthorizedException;
+import org.oa.service.GoodsService;
+import org.oa.util.JWTUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class WebController {
+
+    @Autowired
+    private GoodsService userService2;
+    private static final Logger LOGGER = LogManager.getLogger(WebController.class);
+
+    private UserService userService;
+
+    @Autowired
+    public void setService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/login")
+    public ResponseBean login(@RequestParam("username") String username,
+                              @RequestParam("password") String password) {
+        UserBean userBean = userService.getUser(username);
+        if (userBean.getPassword().equals(password)) {
+            return new ResponseBean(200, "Login success", JWTUtil.sign(username, password));
+        } else {
+            throw new UnauthorizedException();
+        }
+    }
+
+    @GetMapping("/article")
+    public ResponseBean article() {
+        Subject subject = SecurityUtils.getSubject();
+        if (subject.isAuthenticated()) {
+            return new ResponseBean(200, "You are already logged in", null);
+        } else {
+            return new ResponseBean(200, "You are guest", null);
+        }
+    }
+
+    @GetMapping("/require_auth")
+    @RequiresAuthentication
+    public ResponseBean requireAuth() {
+        return new ResponseBean(200, "You are authenticated", null);
+    }
+
+    @GetMapping("/require_role")
+    @RequiresRoles("admin")
+    public ResponseBean requireRole() {
+        return new ResponseBean(200, "You are visiting require_role", null);
+    }
+
+    @GetMapping("/require_permission")
+    @RequiresPermissions(logical = Logical.AND, value = {"view", "edit"})
+    public ResponseBean requirePermission() {
+        return new ResponseBean(200, "You are visiting permission require edit,view", null);
+    }
+
+    @RequestMapping(path = "/401")
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ResponseBean unauthorized() {
+        return new ResponseBean(401, "Unauthorized", null);
+    }
+
+    @GetMapping("he")
+    public Object he() {
+        return "";
+    }
+}
